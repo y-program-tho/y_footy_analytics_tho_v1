e0a599181d705791c98a84bc3bdb4b15e37f655d
y-program-tho <yanga12edu@gmail.com>
2025-11-23 23:36:15 +0000

    Create and place graphs in desired positions

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import gspread
 import pandas as pd
 import streamlit as st
+import altair as alt
 from google.oauth2.service_account import Credentials
 
 # Scopes are used to define the specific operations or accesses you want to have for certain files
@@ -12,9 +13,23 @@ client = gspread.authorize(creds)
 sheet_id = "1Qy_gscrEgy4_msbws4-Mm5ztXuR1bEepxBVlJoq8Egw"
 sheet = client.open_by_key(sheet_id)
 
+# Load data from gspreadsheets
 df = pd.DataFrame(sheet.get_worksheet(0).get_all_records()).set_index('Rk')
 st.write(df)
 
+# Space seperator
+st.markdown('##') 
+
+# EPL Points bar chart with desired sorting
+c = alt.Chart(df).mark_bar().encode(x=alt.X("Squad", sort=None), y="Pts")
+st.altair_chart(c, use_container_width=True)
+
+# Space seperator
+st.markdown('##') 
+
+# Goal scoring perfomance
+st.scatter_chart(df, x="GF", y="xG")
+
+# Place input and display the output
 x = st.text_input("Who is your favourite team?")
 st.write(f"This fan supports: {x}!!!")
-
