da9c0c88751af9abfc9fdb800b529cb169e0c000
y-program-tho <yanga12edu@gmail.com>
2025-11-23 01:37:13 +0000

    Read googlesheet into streamlit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import gspread
+import pandas as pd
+import streamlit as st
 from google.oauth2.service_account import Credentials
 
 # Scopes are used to define the specific operations or accesses you want to have for certain files
@@ -10,5 +12,9 @@ client = gspread.authorize(creds)
 sheet_id = "1Qy_gscrEgy4_msbws4-Mm5ztXuR1bEepxBVlJoq8Egw"
 sheet = client.open_by_key(sheet_id)
 
-values_list = sheet.sheet1.row_values(1)
-print(values_list)
+df = pd.DataFrame(sheet.get_worksheet(0).get_all_records()).set_index('Rk')
+st.write(df)
+
+x = st.text_input("Who is your favourite team?")
+st.write(f"This fan supports: {x}!!!")
+
