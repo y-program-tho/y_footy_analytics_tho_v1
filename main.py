2f317bdcac02e48180e75b91ab40660c4c2cdd27
y-program-tho <yanga12edu@gmail.com>
2025-12-14 23:51:21 +0200

    Add streamlit pages and install more packages

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ st.title("y_footy_analyitics_tho_v1")
 
 data = pd.read_excel("epl_fbref_2425.xlsx", sheet_name="Sheet1")
 st.write(data)
+
+# View of league table
+
+# View of team points tall as bar chart
+
+# View of team goals scored compared to their xG as scatter chart
+
+# View of teams GA compared to their xGA as scatter chart
